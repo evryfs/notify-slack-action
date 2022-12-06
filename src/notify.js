4e19f8fbd1c5a2ecf72e06a4d503a83710f23c12
rainui28 <112956420+rainui28@users.noreply.github.com>
2022-12-06 14:43:02 +0100

    Implements CI using Github Actions (#4)
    - new: test-features.yaml workflow to test action features
    - fix: using attack as error mechanism
    - new: test-errors.yaml workflow to test error cases

--- src/notify.js
+++ src/notify.js
@@ -3,51 +3,51 @@ const needle = require("needle");
 
 const getTemplateEnvs = (str) => str.match(/\$\{?([A-Z0-9_]+)\}?/gm);
 
-const trimTemplateEnvs = (envs) =>
-    envs.map((env) => env.replace(/\$\{?([A-Z0-9_]+)\}?/, "$1"));
+const trimTemplateEnvs = (envs) => envs.map((env) => env.replace(/\$\{?([A-Z0-9_]+)\}?/, "$1"));
 
 const call_slack_api = (payload) => {
-    core.debug(`Payload=${payload}`);
     const options = {
         json: true,
         headers: {
-            Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
-        },
-    };
-    needle.post(
-        "https://slack.com/api/chat.postMessage",
-        payload,
-        options,
-        (error, response) => {
-            if (error) {
-                core.setFailed(error);
-            }
-            core.info("Slack API response");
-            core.info(JSON.stringify(response.body, null, 2));
+            Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`
         }
-    );
+    };
+    needle.post("https://slack.com/api/chat.postMessage", payload, options, (error, response) => {
+        if (error) {
+            core.setFailed(error);
+        } else {
+            core.startGroup("Slack API response");
+            core.info(JSON.stringify(response.body, null, 2));
+            if (response.body.ok)
+                core.info(
+                    `\u001b[32;1mNotification has been delivered in channel ${response.body.channel}\u001b[0m`
+                );
+            else core.setFailed(response.body.error);
+            core.endGroup();
+        }
+    });
 };
 
 const processTemplatesMechanism = (channelId, value) => {
     const templateUrl = `https://raw.githubusercontent.com/evryfs/notify-slack-action/templates/templates/${value}`;
     needle.get(templateUrl, (error, response) => {
         if (error) {
-            core.error(error);
             core.setFailed("Template not found on evryfs/notify-slack-action");
         } else {
-            core.info(
-                `Download template '${value}' from evryfs/notify-slack-action -- ${response.statusMessage}`
-            );
-            let template = JSON.parse(response.body);
-            let blocks = JSON.stringify(template.blocks);
-            let templateEnvs = getTemplateEnvs(blocks);
-            let envs = trimTemplateEnvs(templateEnvs);
-            templateEnvs.forEach((tEnv, i) => {
-                core.debug(`${tEnv}=${process.env[envs[i]]}`);
-                blocks = blocks.replaceAll(tEnv, process.env[envs[i]]);
-            });
-            core.debug(`Blocks=${blocks}`);
-            call_slack_api({ channel: channelId, blocks: blocks });
+            const templateMsgStatus = `Download template '${value}' from evryfs/notify-slack-action -- ${response.statusMessage}`;
+            if (response.statusCode.toString().startsWith(4)) {
+                core.setFailed(templateMsgStatus);
+            } else {
+                core.info(templateMsgStatus);
+                let template = JSON.parse(response.body);
+                let blocks = JSON.stringify(template.blocks);
+                let templateEnvs = getTemplateEnvs(blocks);
+                let envs = trimTemplateEnvs(templateEnvs);
+                templateEnvs.forEach((tEnv, i) => {
+                    blocks = blocks.replaceAll(tEnv, process.env[envs[i]]);
+                });
+                call_slack_api({ channel: channelId, blocks: blocks });
+            }
         }
     });
 };
@@ -63,5 +63,5 @@ const processBlocksMechanism = (channelId, value) => {
 module.exports = {
     blocks: processBlocksMechanism,
     templates: processTemplatesMechanism,
-    text: processTextMechanism,
+    text: processTextMechanism
 };
